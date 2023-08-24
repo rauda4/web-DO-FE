f5edfcc57adedcb98c1b844f0fa1a5801272e9d7
rafikaauda <rafika.auda4@gmail.com>
2023-08-24 23:09:56 +0700

    add product

--- src/Layout/LandingPage.jsx
+++ src/Layout/LandingPage.jsx
@@ -1,6 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Navbar } from '../components/Navbar'
 import { Home } from '../pages/Home'
+import Diamond from '../pages/landingPage/diamond/Diamond'
+import Product from '../pages/landingPage/product/Product'
+import ContactUs from '../pages/landingPage/contactus/ContactUs'
+
+
 
 export const LandingPage = () => {
 
@@ -9,6 +14,9 @@ export const LandingPage = () => {
         <Navbar transparent />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/diamond" element={<Diamond />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/contactus" element={<ContactUs />} />
             <Route path='/*' element={<Navigate to={"/"} />} /> 
         </Routes>
     </>
