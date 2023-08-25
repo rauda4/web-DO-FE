93a32c38fddcde332ccede5efd5f6111cbf44bde
rafikaauda <rafika.auda4@gmail.com>
2023-08-25 21:54:10 +0700

    update diamond page

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 export default function FormRegister() {
-
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -12,79 +11,87 @@ export default function FormRegister() {
     username,
     email,
     password,
-  }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3002/auth/register', payload, {
-        headers : {
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        'http://localhost:8080/auth/register',
+        payload,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      });
-      
+      );
+
       console.log(response);
-    } catch (error) {
-      
-    }
-  }
+    } catch (error) {}
+  };
 
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-        </div>
-        <div className="card flex-shrink-0 w-screen max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='text-center lg:text-left'></div>
+        <div className='card flex-shrink-0 w-screen max-w-sm shadow-2xl bg-base-100'>
+          <div className='card-body'>
             <form onSubmit={handleSubmit}>
-            {/* section username */}
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Username</span>
-              </label>
-              <input 
-              type="text" 
-              placeholder="Username" 
-              name='username' 
-              value={username} 
-              onChange={(e) => setUsername(e.target.value)} 
-              className="input input-bordered"/>
-            </div>
-            {/* section email */}  
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input 
-              type="text" 
-              placeholder="Email" 
-              name='email' 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="input input-bordered" />
-            </div>
-            {/* section Password */}  
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input 
-              type="text" 
-              placeholder="Password" 
-              name='password' 
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input input-bordered" />
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
-              <label className="label justify-center">
-                <a href="/auth/login" className="label-text-alt link link-hover text-blue-800">Already Have Account ? Login Now </a>
-              </label>
-            </div>
+              {/* section username */}
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Username</span>
+                </label>
+                <input
+                  type='text'
+                  placeholder='Username'
+                  name='username'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className='input input-bordered'
+                />
+              </div>
+              {/* section email */}
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Email</span>
+                </label>
+                <input
+                  type='text'
+                  placeholder='Email'
+                  name='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className='input input-bordered'
+                />
+              </div>
+              {/* section Password */}
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Password</span>
+                </label>
+                <input
+                  type='text'
+                  placeholder='Password'
+                  name='password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className='input input-bordered'
+                />
+              </div>
+              <div className='form-control mt-6'>
+                <button className='btn btn-primary'>Sign Up</button>
+                <label className='label justify-center'>
+                  <a
+                    href='/auth/login'
+                    className='label-text-alt link link-hover text-blue-800'>
+                    Already Have Account ? Login Now{' '}
+                  </a>
+                </label>
+              </div>
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/landingPage/diamond/Diamond.jsx
+++ src/pages/landingPage/diamond/Diamond.jsx
@@ -6,45 +6,91 @@ import axios from 'axios';
 export default function Diamond() {
   const [diamond, setDiamond] = useState([]);
 
-  const getDiamond = async () => {
-    const response = await axios.get('http://localhost:8080/diamond');
-    setDiamond(response.data.data);
-  };
   useEffect(() => {
-    getDiamond();
+    const getDiamond = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/diamond');
+        setDiamond(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getDiamond()
   }, []);
 
-  console.log(diamond);
+  // console.log(diamond);
 
   return (
     <>
-      <div className='bg-indigo-950 py-9'></div>
-      <div className='bg-indigo-950 flex justify-center'>
-        {/* <div> */}
-        <div className='grid gap-x-10 gap-y-10 grid-cols-6 my-20'>
-          {diamond.map((item) => {
-            return (
-              <div className='card w-54 bg-white shadow-xl'>
-                <figure className='bg-yellow-500 py-5 glass'>
-                  <img
-                    src={dm}
-                    style={{ height: 80, width: 100 }}
-                    alt='diamond'
-                  />
-                </figure>
-                <div className='card-body'>
-                  <h2 className='text-lg font-medium'>{item.name}</h2>
-                  <p className='text-lg'>
-                    {item.price.toLocaleString('id-ID', {
-                      style: 'currency',
-                      currency: 'IDR',
-                    })}
-                  </p>
-                  <div className='card-actions justify-end'></div>
+      <div className='bg-neutral-900 py-9'></div>
+      <div className='bg-neutral-800 py-24'>
+        {/* content */}
+        <div className=''>
+          <div className='flex justify-center gap-10'>
+            <section>
+              <div className='bg-neutral-900 rounded-xl'>
+                <div className='w-96 py-10 px-10'>
+                  <h1 className='text-white'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum
+                  </h1>
                 </div>
               </div>
-            );
-          })}
+              <div className='bg-neutral-900 rounded-xl mt-5'>
+                <div className='w-96 py-10 px-10'>
+                  <h1 className='text-white'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum
+                  </h1>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <div className='bg-neutral-900 rounded-xl'>
+                <h1 className='flex justify-start ml-10 text-white'>Diamond</h1>
+                <div className='grid gap-x-10 gap-y-10 grid-cols-3 py-10 px-10'>
+                  {diamond.map((item) => {
+                    return (
+                      <div className='card w-44 cursor-pointer hover:bg-sky-700 text-white bg-neutral-800 border border-slate-500 glass shadow-xl'>
+                        <figure className='bg-yellow-400 py-2 glass'>
+                          <img
+                            src={dm}
+                            style={{ height: 80, width: 100 }}
+                            alt='diamond'
+                          />
+                        </figure>
+                        <div className='card-body'>
+                          <h2 className='text-md font-medium'>{item.name}</h2>
+                          <p className='text-md'>
+                            {item.price.toLocaleString('id-ID', {
+                              style: 'currency',
+                              currency: 'IDR',
+                            })}
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div className='text-white flex justify-center'>
+          <div className=''>Cart</div>
         </div>
       </div>
     </>
