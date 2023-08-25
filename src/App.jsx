8d1a2e633a6562b9cbf9831955d1d2ebe4f6f872
rafikaauda <rafika.auda4@gmail.com>
2023-08-25 21:51:51 +0700

    add authonly with token

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { Route, Routes } from "react-router-dom";
-import { AuthPage } from "./Layout/AuthPage";
-import { LandingPage } from "./Layout/LandingPage";
-
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthPage } from './Layout/AuthPage';
+import { LandingPage } from './Layout/LandingPage';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const token = localStorage.getItem('token');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, [token]);
   return (
     <Routes>
-      <Route path="/*" element={<LandingPage/>} />
-      <Route path="/auth/*" element={<AuthPage/>} />
+      <Route
+        path='/*'
+        element={<LandingPage />}
+      />
+      <Route
+        path='/auth/*'
+        element={isAuthenticated ? <Navigate to={'/'} /> : <AuthPage />}
+      />
     </Routes>
   );
 }
