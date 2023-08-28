861437e45cbf6223c3d97b7e6f3cfe32495383f9
rafikaauda <rafika.auda4@gmail.com>
2023-08-28 21:50:00 +0700

    add api authservice login register logout

--- src/feature/auth/authService.js
+++ src/feature/auth/authService.js
@@ -1,19 +1,50 @@
 import axios from 'axios';
-
+import jwtDecode from 'jwt-decode';
+// Register user
 const register = async (userData) => {
   const response = await axios.post(
-    'http://localhost:3002/auth/register',
-    userData
+    'http://localhost:8080/auth/register',
+    userData,
   );
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
+
   return response.data;
+};
+
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(
+    'http://localhost:8080/auth/login',
+    userData,
+  );
+  const accesToken = response.data.token;
+  if (response.data.auth === true) {
+    localStorage.setItem('token', accesToken);
+    const userAccount = jwtDecode(accesToken);
+    localStorage.setItem('userId', userAccount.id);
+    localStorage.setItem('username', userAccount.username);
+    localStorage.setItem('email', userAccount.email);
+  }
+
+  return response.data;
+};
+
+// Logout user
+const logout = () => {
+  localStorage.removeItem('username');
+  localStorage.removeItem('userId');
+  localStorage.removeItem('token');
+  localStorage.removeItem('email');
+  window.location.reload();
 };
 
 const authService = {
   register,
+  logout,
+  login,
 };
 
 export default authService;
